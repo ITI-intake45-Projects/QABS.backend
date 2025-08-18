16ea863f8657e1076d668938ec97162ee002ba73
Osamatalaat <osamatalaat707@gmail.com>
2025-08-18 14:52:01 +0300

    serviceResult

--- QBS.Models/Entities/PayoutItem.cs
+++ QBS.Models/Entities/PayoutItem.cs
@@ -4,7 +4,7 @@ namespace QABS.Models
     public class PayoutItem
     {
         public int Id { get; set; }
-        public decimal Amount { get; set; }// price for teacher
+        public decimal Amount { get; set; }// price for teacher Amount = hourlyrate * 40 /60(session duration) 
         public int TeacherPayoutId { get; set; }
         public TeacherPayout TeacherPayout { get; set; } = default!;
         public int SessionId { get; set; }
