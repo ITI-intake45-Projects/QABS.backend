44627d9fe19a39b3a7a24f436ad21b09af8049f0
Mahmoud <mahmoud.m.sayed20@gmail.com>
2025-08-18 14:51:07 +0300

    session viewmodel done

--- QABS.ViewModels/Session/SessionEditVM.cs
+++ QABS.ViewModels/Session/SessionEditVM.cs
@@ -4,5 +4,7 @@ namespace QABS.ViewModels
 {
     public class SessionEditVM
     {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
     }
 }
--- QABS.ViewModels/Session/SessionExt.cs
+++ QABS.ViewModels/Session/SessionExt.cs
@@ -1,7 +1,35 @@
 ﻿
+using QABS.Models;
+
 namespace QABS.ViewModels
 {
     public static class SessionExt
     {
+        public static Session ToCreate(this SessionCreateVM vm)
+        {
+            return new Session
+            {
+                StartTime = vm.StartTime,
+                EnrollmentId = vm.EnrollmentId
+            };
+        }
+        public static SessionDetailsVM ToDetails(this Session session)
+        {
+            return new SessionDetailsVM
+            {
+                Id = session.Id,
+                StartTime = session.StartTime,
+                Status = session.Status,
+                EnrollmentId = session.EnrollmentId,
+                PayoutItemsDetails = session.PayoutItems.Select(p => p.ToDetails()).ToList()
+            };
+        }
+        public static Session ToEdit(this SessionEditVM newsession, Session OldSession)
+        {
+
+            OldSession.StartTime = newsession.StartTime == default ? OldSession.StartTime : newsession.StartTime;
+
+            return OldSession;
+        }
     }
 }
